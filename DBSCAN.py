cec200ce4c9221171576377b1d4941dd79c367df
abderrahman504 <rahmankhsherb@gmail.com>
2024-03-27 04:52:51 +0200

    Fixes and optimizations to DBSCAN fix sometimes an outlier is unclustered, the datatype of the clustering array is int instead of float, return outlier count.

--- DBSCAN.py
+++ DBSCAN.py
@@ -1,58 +1,49 @@
 import numpy as np
-
-
-def euclid_distance_matrix(data):
-    mat = np.zeros((data.shape[0], data.shape[0]))
-    for i in range(data.shape[0]):
-        for j in range(i+1, data.shape[0]):
-            mat[i, j] = np.linalg.norm(data[i] - data[j])
-            mat[j, i] = mat[i, j]
-    return mat
-
-
+from sklearn.metrics import pairwise_distances
 
 def DBSCAN(data, eps, min_pts):
-    adj_mat = euclid_distance_matrix(data)
+    adj_mat = pairwise_distances(data, metric='euclidean')
     n = adj_mat.shape[0]
-    clustering = np.zeros(n)
+    clustering = np.zeros(n, dtype=np.int64)
     core = set()
     neighborhoods = []
-    #Find core points
+    #Find core points and neighborhoods
     for i in range(n):
         clustering[i] = -1
-        neighborhoods.append(np.where(adj_mat[i] <= eps)[0])
+        neighbors = np.where(adj_mat[i] <= eps)[0]
+        neighbors = neighbors[neighbors != i]
+        neighborhoods.append(neighbors)
         if len(neighborhoods[i]) >= min_pts:
             core.add(i)
-            
     #Expand clusters
     k = 0
     for x_idx in core:
         if clustering[x_idx] != -1:
             continue
         clustering[i] = k
-        k += 1
         #Add density connected points to cluster
         density_connected(x_idx, k, neighborhoods, clustering, core)
-    
+        k += 1
     #Clustering outliers
     outlier_count = 0
     for i in range(n):
         if clustering[i] == -1:
             outlier_count += 1
-            nearest = 0
-            for j in range(1, n):
+            nearest = -1
+            for j in range(0, n):
                 if clustering[j] != -1:
-                    if adj_mat[i, j] < adj_mat[i, nearest]: nearest = j
+                    if nearest == -1 or adj_mat[i, j] < adj_mat[i, nearest]: nearest = j
             clustering[i] = clustering[nearest]
     print(f"DBSCAN: {outlier_count} outliers were clustered with 1NN.")
-    
-    return clustering, k #Clustered data, number of clusters         
+    return clustering, k, outlier_count #Clustered data, number of clusters  , number of outliers       
 
 
 def density_connected(x_idx, k, neighborhoods, clustering, core):
-    for j in neighborhoods[x_idx]:
-            if clustering[j] == -1:
-                clustering[j] = k
-                if j in core:
-                    density_connected(j, k, neighborhoods, clustering, core)
-    
+    queue = [(x_idx, k)]
+    while len(queue) > 0:
+        x_idx, k = queue.pop(0)
+        for j in neighborhoods[x_idx]:
+                if clustering[j] == -1:
+                    clustering[j] = k
+                    if j in core:
+                        queue.append((j, k))
